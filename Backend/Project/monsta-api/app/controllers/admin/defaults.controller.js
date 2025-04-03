8e6d037ebf0671591bde8d2ea5ecda54eb1e1c19
pradeeprai1990 <pradeep.9997@gmail.com>
2025-04-03 21:52:36 +0530

    Project Setup

---
+++ Backend/Project/monsta-api/app/controllers/admin/defaults.controller.js
@@ -0,0 +1,148 @@
+const defaultSchema = require('../../models/default');
+const mongodb = require('mongodb');
+
+//  For Create Data
+exports.create = async(request,response) => {
+
+    var dataSave = {
+        name : request.body.name,
+        order : request.body.order,
+    }
+    
+    await new defaultSchema(dataSave).save()
+    .then((resp) => {
+        const result = {
+            _status : true,
+            _message : 'Record inserted succussfully',
+            _data :  resp
+        }
+    
+        response.send(result);
+    })
+    .catch((error) => {
+        var error_messages = [];
+        var errorKey = {};
+
+        for(var data in error.errors){
+            errorKey[data] = error.errors[data].properties.message;
+            error_messages.push(errorKey);
+        }
+
+        const result = {
+            _status : false,
+            _message : 'Something went wrong !!',
+            error_messages : error_messages,
+            _data :  null
+        }
+    
+        response.send(result);
+    })
+}
+
+//  For View Data
+exports.view = async(request,response) => {
+    
+    var limit = 15;
+    var page = 1;
+
+    if(request.body.limit != '' && request.body.limit != undefined){
+        limit = request.body.limit;
+    }
+
+    if(request.body.page != '' && request.body.page != undefined){
+        page = request.body.page;
+    }
+
+    skip = (page - 1) * limit;
+
+    const condition = {
+        deleted_at : null,
+    }
+
+    if(request.body.name != '' && request.body.name != undefined){
+        var nameRegex = new RegExp(request.body.name,"i");
+        condition.name = nameRegex;
+    }
+    
+    await defaultSchema.find(condition).select('name status order')
+    .limit(limit).skip(skip)
+    .sort({
+        _id : 'desc'
+    })
+    .then((resp) => {
+        if(resp.length > 0){
+            const result = {
+                _status : true,
+                _message : 'Record inserted succussfully',
+                _data :  resp
+            }
+        
+            response.send(result);
+        } else {
+            const result = {
+                _status : false,
+                _message : 'No record found.',
+                _data :  []
+            }
+        
+            response.send(result);
+        }
+        
+    })
+    .catch(() => {
+        const result = {
+            _status : false,
+            _message : 'Something went wrong !!',
+            _data :  null
+        }
+    
+        response.send(result);
+    })
+}
+
+//  For Details Data
+exports.details = (request,response) => {
+
+}
+
+//  For Update Data
+exports.update = (request,response) => {
+
+}
+
+//  For Change Status Data
+exports.changeStatus = (request,response) => {
+
+}
+
+//  For Delete Data
+exports.destroy = async(request,response) => {
+
+    await defaultSchema.updateOne(
+        {
+            _id : request.body.id
+        },
+        {
+            $set : {
+                deleted_at : Date.now()
+            }
+        }
+    ).then((result) => {
+        const data = {
+            _status : true,
+            _message : 'Record updated succussfully',
+            _data :  result
+        }
+    
+        response.send(data);
+
+    }).catch((error) => {
+        const data = {
+            _status : false,
+            _message : 'Something went wrong !!',
+            _data :  ''
+        }
+    
+        response.send(data);
+    })
+}
